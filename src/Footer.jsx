4b34a274f27624cfdb7e181e45074ab0972a48ff
Chitranjan kumar <chitranjan75075@gmail.com>
2025-10-02 23:28:31 +0530

    Update Footer.jsx

--- src/Footer.jsx
+++ src/Footer.jsx
@@ -1,1 +1,11 @@
+import React from "react";
+function Footer(){
+    return(
+        <React.Fragment>
+            <h1>Welcome footer</h1>
+        </React.Fragment>
+    )
+}
+export default Footer;
+
 
